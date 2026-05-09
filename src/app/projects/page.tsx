f8c3a2e30087eef9fc929570ecad0c39dc30fa28
z5569304 <z5569304@unsw.edu.au>
2026-05-09 13:59:59 +1000

    feat: protect projects route

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,18 @@
 import { ProjectListPage } from "@/features/projects/components/ProjectListPage";
+import { requireCurrentUser } from "@/server/auth/session";
+import { listProjectsForCurrentUser } from "@/server/projects/projectService";
 
-export default function ProjectsRoute() {
-  return <ProjectListPage />;
+export const dynamic = "force-dynamic";
+
+export default async function ProjectsRoute() {
+  const user = await requireCurrentUser("/projects");
+  const projects = await listProjectsForCurrentUser();
+
+  return (
+    <ProjectListPage
+      authUserEmail={user.email}
+      projectCount={projects.length}
+      projects={projects}
+    />
+  );
 }
